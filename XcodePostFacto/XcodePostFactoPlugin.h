f84ae02d6e7cf5b15aa48c4419e3af1161e9bd6a
Landon Fuller <landonf@plausible.coop>
2015-03-14 15:55:37 -0600

    Stub out a basic plugin based on the excode-plugin implementation.

---
+++ XcodePostFacto/XcodePostFactoPlugin.h
@@ -0,0 +1,13 @@
+//
+//  XCodePostFactoPlugin.h
+//  XCodePostFacto
+//
+//  Created by Landon Fuller on 3/14/15.
+//  Copyright (c) 2015 Landon Fuller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XCodePostFactoPlugin : NSObject
+
+@end
---
+++ XcodePostFacto/XcodePostFactoPlugin.m
@@ -0,0 +1,81 @@
+/*
+ * Author: Landon Fuller <landon@landonf.org>
+ *
+ * Copyright (c) 2015 Landon Fuller <landon@landonf.org>
+ * Copyright (c) 2013 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#import "XCodePostFactoPlugin.h"
+#import "XPFLog.h"
+
+
+@implementation XCodePostFactoPlugin {
+@private
+}
+
+// force loading of the plugin class upon bundle load
++ (void) load {
+    [self defaultPlugin];
+}
+
+/**
+ * Return the default plugin instance.
+ */
++ (instancetype) defaultPlugin {
+    static XCodePostFactoPlugin *defaultInstance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        defaultInstance = [[self alloc] init];
+    });
+    
+    return defaultInstance;
+}
+
+/**
+ * Initialize a new instance.
+ */
+- (instancetype) init {
+    if ((self = [super init]) == nil)
+        return nil;
+    
+    XPFLog(@"Plugin loaded; starting up ...");
+    
+    return self;
+}
+
+// Returns the handler for a given action. This seemingly should already be available via
+// the entries 'action' value; we need to spend some time reverse engineering the menu configuration
+// options and how actions are mapped.
++ (id) handlerForAction: (SEL) action withSelectionSource: (id) src {
+    return [self defaultPlugin];
+}
+
+// XCPF.CmdHandler.ViewAnalyzerMenuEntry handler.
+- (void) enableViewAnalyzer: (id) sender {
+    // TODO
+}
+
+
+@end
